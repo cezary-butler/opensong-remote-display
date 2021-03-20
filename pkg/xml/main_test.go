09775d3f363c4d103f9b9de6c6eb63df420e453f
Cezary Butler <cezary.butler@programisci.eu>
2021-03-20 14:45:31 +0100

    fixed test

--- pkg/xml/main_test.go
+++ pkg/xml/main_test.go
@@ -1,6 +1,7 @@
-package main
+package xml
 
 import (
+	"bitbucket.org/cezary_butler/opensong-remote-display/pkg/opensong"
 	"github.com/stretchr/testify/assert"
 	"golang.org/x/net/websocket"
 	"testing"
@@ -8,7 +9,7 @@ import (
 
 func Test_messageCanBeUnmarshaled(t *testing.T) {
 
-	var result Response
+	var result opensong.Response
 	msg := "<?xml version=\"1.0\" encoding=\"UTF-8\"?><response resource=\"presentation\" action=\"status\"><presentation running=\"1\"><screen mode=\"N\">normal</screen><slide itemnumber=\"12\"><name>And Can It Be</name><title>And Can It Be</title></slide></presentation></response>"
 	xmlUnmarshal([]byte(msg), websocket.TextFrame, &result)
 
